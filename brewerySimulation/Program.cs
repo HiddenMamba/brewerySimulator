99a7fdf39051e061d316a035d5e60ad2767916f9
krzyKrzysztofx <krzysztofxdurczynski@gmail.com>
2019-05-19 19:32:48 +0200

    Initial

---
+++ brewerySimulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace brewerySimulation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
